6574f3f7126fc3119f0ce8c53158f1432392f633
Will Simpson <will@kestrelcreek.com>
2024-08-20 14:43:23 -0700

    Refactor code to insert second-level header and extract note

--- Installed/will.insert_header.thearchiveplugin/main.js
+++ Installed/will.insert_header.thearchiveplugin/main.js
@@ -1,28 +1,7 @@
+
+// text selection
 const selectedText = input.text.selected;
-// Get current date and time
-// const now = new Date();
-
-// // Format the date and time to a 12-digit string
-// const timestamp = now.getFullYear().toString() +
-//                   (now.getMonth() + 1).toString().padStart(2, '0') +
-//                   now.getDate().toString().padStart(2, '0') +
-//                   now.getHours().toString().padStart(2, '0') +
-//                   now.getMinutes().toString().padStart(2, '0');
-
 const second_level_header = `## ${selectedText}`;
 
-// // Move selected text to a new file.
-// //const targetFilename = output.newFile.filename;
-// const targetContent = [
-//   `# ${targetFilename}`,
-//   "",
-//   selectedText,
-//   "" 
-// // Ensure there's a trailing newline to be a good citizen of plain text files.
-// ].join("\n");
-// output.newFile.setContent(targetContent);
-
-
-// Removed for testing
-// Replace selection with link.
+// Replace selection with Second Level Header
 output.insert.setText(`${second_level_header}`);
